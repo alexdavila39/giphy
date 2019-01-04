799594811e55a0d07b419d9949bd109d487c3fe4
Alejandro <alexdavila39@gmail.com>
2019-01-04 17:54:38 -0500

    Working unstyled  Code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# giphy
+# Create an App that allows user to search, play/pause Gif's
+
+* create a topics array with ten items
+* Create a function that will create the buttons inside the array using a for loop. Make sure to use the .empty method
+* get an api key form the Giphy website
+* use .ajax to grab all the Gifs from the giphys api 
+* 
--- assets/giphy.js
+++ assets/giphy.js
@@ -1,31 +1,31 @@
 $(function(){
     createButtons(topics, "searchButton", "#buttons");
-console.log("load page");
+//console.log("load page");
 })
 
 var topics = ["Fresh Prince","Michael Scott","Barney Stinson","Adam Sandler","Kevin James","jonah Hill","Eddy murphy","jerry seinfeld","Michael Kelso", "Jim Carrey"];
 
 function createButtons (topics, classToAdd, areaToAddTo){
-    $(areaToAddTo).empty();
+    $(areaToAddTo).empty(); //why do i need to empty areaToAddTo? 
     for (var i = 0; i < topics.length;i++){
-        var bttn = $("<button>");
-        bttn.addClass(classToAdd);
-        bttn.attr("data-type",topics[i]);
-        bttn.text(topics[i]);
-        $(areaToAddTo).append(bttn);
+        var a = $("<button>");
+        a.addClass(classToAdd);
+        a.attr("data-type",topics[i]);
+        a.text(topics[i]); //writes the name on the buttons
+        $(areaToAddTo).append(a);
     }
 }
 
 $(document).on("click", ".searchButton", function(){
-    $("#searches").empty();
+    $("#searches").empty(); //empties the search-form 
     var type = $(this).data("type");
-    console.log(type);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=XaAzD07hk1kwfKsoN2w7LErp50qOyG0e&limit=10";
+   // console.log(type);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=XaAzD07hk1kwfKsoN2w7LErp50qOyG0e&limit=10&rating=R";
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-    .done(function(response){
+    .then(function(response){
         console.log(response);
         for (var i = 0; i < response.data.length; i++){
             var searchDiv = $("<div class = 'search-item'>");
@@ -38,7 +38,7 @@ $(document).on("click", ".searchButton", function(){
             image.attr("data-still", still);
             image.attr("data-animated", animated);
             image.attr("data-state","still");
-            image.addClass("searchGif");
+            image.addClass("searchImage");
             searchDiv.append(p);
             searchDiv.append(image);
             $("#searches").append(searchDiv);
@@ -47,24 +47,24 @@ $(document).on("click", ".searchButton", function(){
 
 })
 
-$(document).on("click", ".searchGif", function(){
+$(document).on("click", ".searchImage", function(){
     var state = $(this).attr("data-state");
-    if (state == "still"){
+    if (state === "still"){
         $(this).attr("src", $(this).data("animated"));
         $(this).attr("data-state", "animated");
     } else {
         $(this).attr("src", $(this).data("still"));
         $(this).attr("data-state", "still");
     }
-})
+});
 
 $("#addSearch").on("click", function(){
     var newSearch = $("input").eq(0).val();
     topics.push(newSearch);
-    createButtons(topics, ".searchButton", "#buttons");
-    return false;
+    createButtons(topics, "searchButton", "#buttons");
+    return false;//why return false?
+    
 });
-
 
 
 
